197f7ccaa27537e020ac91ae0a55882385da255e
Flaviu Hossu <flaviu.hossu@gmail.com>
2021-09-11 14:20:30 +0300

    navbar almost done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-const bars = document.querySelector('.toggle-collapse');
+const bars = document.querySelector('.toggle-collapse')
 const nav = document.querySelector('.nav')
 
-bars.addEventListener('click',(event)=>{
-nav.classList.toggle('toggle');
+bars.addEventListener('click', (event) => {
+  nav.classList.toggle('toggle')
+})
+
+// in cazul in care Toggle ramane facut si se face resize peste media querry.
+// TODO: sa nu hardcodezi acel 830. vezi cum poti face
+window.addEventListener('resize', (event) => {
+  console.log(window.innerWidth)
+  if (window.innerWidth > 830) {
+    nav.classList.remove('toggle')
+  }
 })
